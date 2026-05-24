f74f05109e34ea7e7dad24fa87526aa6fea3ba61
H.Farajvand <hadifarajvand3@gmail.com>
2026-05-25 01:15:05 +0330

    Feature 055: add smoke run package

---
+++ src/analysis/paper_default_training_smoke_run/__init__.py
@@ -0,0 +1,8 @@
+"""Paper-default training smoke run analysis package."""
+
+from .runner import generate_paper_default_training_smoke_artifacts, run_paper_default_training_smoke
+
+__all__ = [
+    "generate_paper_default_training_smoke_artifacts",
+    "run_paper_default_training_smoke",
+]
