6b4dbf082a9694b44ab664d024ddbf67bfcaec38
H.Farajvand <hadifarajvand3@gmail.com>
2026-07-16 16:59:33 +0330

    HOODIE: export destination-aware learner

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -4,6 +4,7 @@ from .ddqn import (
     ReplayBuffer as DdqnReplayBuffer,
     Transition as DdqnTransition,
 )
+from .destination_recurrent_ddqn import DestinationRecurrentDoubleDQNAgent
 from .distributed_hoodie import DistributedHoodiePolicy
 from .hoodie_agent import HoodieAgent
 from .lstm_dueling_dqn import LSTM_Dueling_DQN
@@ -11,6 +12,7 @@ from .recurrent_ddqn import RecurrentDDQNLearner, RecurrentDoubleDQNAgent
 
 __all__ = [
     "DDQNLearner",
+    "DestinationRecurrentDoubleDQNAgent",
     "DuelingQNetwork",
     "DdqnReplayBuffer",
     "DdqnTransition",
